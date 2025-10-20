17bd622c79846e848b0ff457936b979e9823980e
albert <swiftfrog@gmail.com>
2025-10-20 16:53:16 +0800

    fixed Plugin.cs

--- Plugin.cs
+++ Plugin.cs
@@ -3,6 +3,7 @@ using MediaBrowser.Common.Configuration; // IApplicationPaths
 using MediaBrowser.Common.Plugins; // IPlugin, BasePlugin
 using MediaBrowser.Model.Plugins; // BasePluginConfiguration
 using MediaBrowser.Model.Serialization; // IXmlSerializer
+using Microsoft.Extensions.DependencyInjection; // IServiceCollection
 using System;
 
 namespace EmbyMedia.Plugin;
@@ -14,13 +15,11 @@ public class PluginConfiguration : BasePluginConfiguration
     // public bool EnableStrmProbing { get; set; } = true;
 }
 
-// 2. 让 Plugin 继承 BasePlugin<PluginConfiguration> 并实现 IPlugin
-public class Plugin : BasePlugin<PluginConfiguration>, IPlugin // Implement IPlugin for good measure
+// 2. 让 Plugin 继承 BasePlugin<PluginConfiguration>, 实现 IPlugin, 并实现 IPluginServiceRegistrator
+public class Plugin : BasePlugin<PluginConfiguration>, IPlugin, IPluginServiceRegistrator // Implement IPluginServiceRegistrator
 {
     public Plugin(IApplicationPaths applicationPaths, IXmlSerializer xmlSerializer) : base(applicationPaths, xmlSerializer)
     {
-        // Plugin Instance can be accessed here if needed, though less common in modern plugins
-        // Instance = this; // If you have a static Instance property elsewhere, assign it here
     }
 
     public override string Name => "EmbyMedia"; // 插件显示名称
@@ -28,4 +27,13 @@ public class Plugin : BasePlugin<PluginConfiguration>, IPlugin // Implement IPlu
     public override Guid Id => Guid.Parse("35F12540-9EBD-9146-8E44-5D6D9BD66489"); // 您的唯一插件 GUID
 
     public override string Description => "Creates .mediainfo backups for media items and restores them.";
+
+    // 3. 实现 IPluginServiceRegistrator 接口的 RegisterServices 方法
+    public void RegisterServices(IServiceCollection serviceCollection)
+    {
+        // 注册 ICustomMetadataProvider<Video> 实现
+        serviceCollection.AddSingleton<ICustomMetadataProvider<Video>, MediaInfoCustomMetadataProvider>();
+        // 如果有其他服务需要注册，也可以在这里添加
+        // serviceCollection.AddSingleton<IMediaInfoService, MediaInfoService>();
+    }
 }
