32d9fb1868d4c5e19d2ebd2357b5d6273285115b
albert <swiftfrog@gmail.com>
2025-10-22 21:43:40 +0800

    s1.1.12

--- Configuration/PluginConfiguration.cs
+++ Configuration/PluginConfiguration.cs
@@ -1,11 +1,18 @@
 // Configuration/PluginConfiguration.cs
-using MediaBrowser.Model.Plugins;
+// 1. 移除旧的 using 语句
+// using MediaBrowser.Model.Plugins; 
+
+// 2. 引入新的、必需的 using 语句
+using Emby.Web.GenericEdit;
 using System.ComponentModel;
 
-namespace EverMedia.Configuration; // 使用命名空间组织代码
+namespace EverMedia.Configuration;
 
-public class PluginConfiguration : BasePluginConfiguration
+// 3. ✅ 关键修订: 更改基类
+// 将 BasePluginConfiguration 更改为 EditableOptionsBase
+public class PluginConfiguration : EditableOptionsBase
 {
+    // 你的所有配置项属性保持不变
     public string BackupMode { get; set; } = "SideBySide";
     public string CentralizedRootPath { get; set; } = "";
     public bool EnableSelfHealing { get; set; } = true;
@@ -13,4 +20,3 @@ public class PluginConfiguration : BasePluginConfiguration
     public bool EnableOrphanCleanup { get; set; } = false;
     public string LogLevel { get; set; } = "Info";
 }
-
